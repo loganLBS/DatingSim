6421b4a3c3ebb832c82d03f18f250eea1ce89c2f
WilleBoi07 <jonssonw07@outlook.com>
2026-03-03 14:04:04 +0100

    Update SaveFunction.cs

--- DatingSim/Assets/Scripts/SaveFunction.cs
+++ DatingSim/Assets/Scripts/SaveFunction.cs
@@ -6,20 +6,20 @@ public static class SaveFunction
 {
     private static string savePath => Application.persistentDataPath + "/save.json";
 
-    public static void Save(Dag1)
+    public static void Save(SaveInfo Dag1)
     {
         SaveInfo data = new SaveInfo
         {
             //seed = generator.seed,
             //playerPosition = player.transform.position,
 
-            Day = //VS.Day
-            cassieTrust = //VS.Cassie
-            bebeTrust = //VS.Bebe
-            nancyTrust = //VS.Nancy
-            emikoTrust = //VS.Emiko
-            takikoTrust = //VS.Takiko
-            sashaTrust =  //VS.Sasha
+            Day = Dag1.Day,
+            cassieTrust = Dag1.cassieTrust,
+            bebeTrust = Dag1.bebeTrust,
+            nancyTrust = Dag1.nancyTrust,
+            emikoTrust = Dag1.emikoTrust,
+            takikoTrust = Dag1.takikoTrust,
+            sashaTrust =  Dag1.sashaTrust,
         };
 
         string json = JsonUtility.ToJson(data, true);
